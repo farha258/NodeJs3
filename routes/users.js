06d59f89d1f163f0458e53f24a125f212c46109d
Farha Maskuriy <farha.maskuriy@gmail.com>
2020-02-29 02:10:06 +0800

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+const authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -17,7 +18,7 @@ router.post('/signup', (req, res, next) => {
       if(err) {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.jason({err: err});
+        res.json({err: err});
       }
     else {
       passport.authenticate('local')(req, res, () => {
@@ -30,10 +31,13 @@ router.post('/signup', (req, res, next) => {
 });
 
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+router.post('/login', passport.authenticate('local'), (req, 
+  res) => {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, 
+      status: 'You are successfully logged in!'});
 });
 
 
