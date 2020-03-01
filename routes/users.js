e1a2b860830918b8d3d3939f0d1eb4fed245c9b8
Farha Maskuriy <farha.maskuriy@gmail.com>
2020-03-02 03:11:51 +0800

    Assignment 3.1

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
-  res.send('respond with a resource');	  User.find({})
+  User.find({})
   .then((users) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
