31eac3e7eebad317531d91090450c86b4d40db9c
Farha Maskuriy <farha.maskuriy@gmail.com>
2020-03-04 00:45:25 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '832554583887271',
+        clientSecret: '74d9f1e625ee12805c3819aee5c530fd'
+    }
 }
